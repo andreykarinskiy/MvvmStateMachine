24e846917fcbc4896841889a2bf65576523f5e26
Andrey Karinskiy <a.karinskiy@hotmail.com>
2016-05-09 22:52:18 +0300

    1

---
+++ MvvmFsm/ViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace MvvmFsm
+{
+    public class ViewModel : BindableBase
+    {
+        private readonly IDictionary<string, object> properties = new Dictionary<string, object>();
+
+        protected dynamic Get([CallerMemberName] string propertyName = default(string))
+        {
+            // ReSharper disable once ExplicitCallerInfoArgument
+            return Get<dynamic>(propertyName);
+        }
+
+        protected virtual T Get<T>([CallerMemberName] string propertyName = default(string))
+        {
+            if (string.IsNullOrWhiteSpace(propertyName))
+            {
+                throw new ArgumentException("propertyName");
+            }
+
+            object value;
+            if (properties.TryGetValue(propertyName, out value))
+            {
+                return (T) value;
+            }
+
+            return default(T);
+        }
+
+        protected virtual void Set<T>(T value, [CallerMemberName] string propertyName = default(string))
+        {
+            if (string.IsNullOrWhiteSpace(propertyName))
+            {
+                throw new ArgumentException("propertyName");
+            }
+
+            properties[propertyName] = value;
+        }
+    }
+}
---
+++ MvvmFst.Tests/ViewModelTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MvvmFsm;
+using NUnit.Framework;
+
+namespace MvvmFst.Tests
+{
+    [TestFixture]
+    public class ViewModelTests
+    {
+        private SimpleViewModel sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new SimpleViewModel();
+        }
+
+        [Test]
+        public void WhenPropertyHasNotBeenSpecifiedThenReturnsDefaultValue()
+        {
+            // arrange
+
+            // act
+            var value = sut.Name;
+
+            // assert
+            value.Should().BeNullOrEmpty();
+        }
+
+        [Test]
+        public void WhenPropertyHasBeenSetThenItReturnsValue()
+        {
+            // arrange
+            sut.Name = "Batygin";
+
+            // act
+            var value = sut.Name;
+
+            // assert
+            value.Should().Be("Batygin");
+        }
+    }
+
+    public class SimpleViewModel : ViewModel
+    {
+        public string Name
+        {
+            get { return Get(); }
+            set { Set(value); }
+        }
+    }
+}
